4befae06236db27c4eb27d4b3817de218786a3da
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 03:32:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organizedData = Hash.new
+  data.each do |attributeType, attributeValues |
+    attributeValues.each do |attributeValue, birdNames|
+      birdNames.each do |bird|
+        organizedData[bird] = {} unless organizedData[bird]
+        organizedData[bird][attributeType] = [] unless organizedData[bird][attributeType]
+        organizedData[bird][attributeType].push(attributeValue.to_s)
+      end
+    end
+  end
+  return organizedData
 end
