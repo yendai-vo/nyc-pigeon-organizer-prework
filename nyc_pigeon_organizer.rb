cbbc26add6f1cb8ac997030515adbca62e72578b
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 03:26:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,10 @@ def nyc_pigeon_organizer(data)
     attributeValues.each do |attributeValue, birdNames|
       birdNames.each do |bird|
         organizedData[bird] = {} unless organizedData[bird]
-        organizedData[bird][attributeType] = [attributeValue]
+        organizedData[bird][attributeType] = [] unless organizedData[bird][attributeType]
+        
+        organizedData[bird][attributeType].push(attributeValue)
+        # organizedData[bird][attributeType] = [attributeValue]
         # organizedData[bird] = {} unless organizedData[bird]
         # organizedData[bird][attributeType] = [] unless organizedData[bird][attributeType]
         # organizedData[bird][attributeType].push(attributeValue.to_s)
