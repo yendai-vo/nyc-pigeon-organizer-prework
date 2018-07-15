cb6c665a0669a9a4e68eb18efc292994ebfb582e
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 02:37:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organizedData = {}
+  data.each do |attributeType, propertyHash |
+    propertyHash.each do |attributeValue, birdName|
+      organizedData[birdName] = {
+        attributeType => []
+      }
+      organizedData[birdName][attributeType].push(attributeValue.to_s)
+    end
+  end
 end
